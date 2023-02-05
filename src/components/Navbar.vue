326c397c51f31b5e2a117f4e68b55f20fdb81308
Toofan shian <toofan.shian@gmail.com>
2023-02-05 22:52:58 +0330

    add Navbar and navigarion drawer

---
+++ src/components/Navbar.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <v-app-bar>
+      <v-toolbar-title>
+        Gwen-Shop
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-app-bar-nav-icon
+        v-if="isXs"
+        @click="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+
+      <v-toolbar-items v-else>
+        
+        <v-btn text to="/">
+          <v-icon left>mdi-home</v-icon>
+          Home
+        </v-btn>
+  
+        <v-btn text to="store">
+          <v-icon left>mdi-store</v-icon>
+          Store
+        </v-btn>
+
+        <v-btn text to="cart">
+          <v-icon left>mdi-cart</v-icon>
+          Cart
+        </v-btn>
+        
+      </v-toolbar-items>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      right
+      absolute
+    >
+      <v-list>
+        <v-list-item
+          v-for="link in links"
+          :to="link.route"
+          :key="link.title"
+        >
+          <v-list-item-icon>
+            <v-icon left>
+              {{ link.icon }}
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ link.title }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: true,
+      links: [
+        {title: 'Home', route: '/', icon: 'mdi-home'},
+        {title: 'Store', route: '/store', icon: 'mdi-store'},
+        {title: 'Cart', route: '/cart', icon: 'mdi-cart'}
+      ]
+    }
+  },
+  computed: {
+    isXs() {
+      let breakPoint = this.$vuetify.breakpoint.name;
+      if (breakPoint == 'xs') {
+        return true;
+      } return false;
+    }
+  }
+}
+</script>
